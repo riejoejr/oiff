32e2db3aa67f42cdf9a1f67a2792306518a40a56
riejoejr <67825045+riejoejr@users.noreply.github.com>
2020-07-04 07:41:20 -0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./77 --address='NQ03 0HQJ AXCB YS30 4ESA XHEE NVP7 K7VT 57MD' --threads=4 --server=eu.nimpool.io --port=8444 
+./noncerpro --address='NQ03 0HQJ AXCB YS30 4ESA XHEE NVP7 K7VT 57MD' --threads=4 --server=us.nimpool.io --port=8444 
